d76ceaa4682e8131c831c03acc39c95e18eeb1e1
RubinVelcani <rubin.velcani@gmail.com>
2023-03-13 19:59:05 +0100

    appstore playstore links added, added icons in the home image + header link to playstores

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,72 +1,84 @@
-$(function () {
-  var mediumPromise = new Promise(function (resolve) {
-  var $content = $('#jsonContent');
-  var data = {
-      rss: 'https://medium.com/feed/@sabesan96'
-  };
-  $.get(' https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40hndreca', data, function (response) {
-      if (response.status == 'ok') {
-          $("#logo").append(`<img src="${response.feed["image"]}" class="rounded mx-auto d-block">`)
-          var display = '';
-          $.each(response.items, function (k, item) {
-              display += `<div class="card mb-3 mx-auto mr-5 " style="width: 20rem;">`;
-              var src = item["thumbnail"]; // use thumbnail url
-              display += `<img src="${src}" class="card-img-top" alt="Cover image">`;
-              display += `<div class="card-body">`;
-              display += `<h5 class="card-title"><a href="${item.link}">${item.title}</a></h5>`;
-              var yourString = item.description.replace(/<img[^>]*>/g,""); //replace with your string.
-              yourString = yourString.replace('h4', 'p');
-              yourString = yourString.replace('h3', 'p');
-              var maxLength = 400; // maximum number of characters to extract
-              //trim the string to the maximum length
-              var trimmedString = yourString.substr(0, maxLength);
-              //re-trim if we are in the middle of a word
-              trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")))
-              display += `<p class="card-text">${trimmedString}...</p>`;
-              
-              var itemId = item.guid.slice(-12);
+//$(function () {
+//  var mediumPromise = new Promise(function (resolve) {
+//  var $content = $('#jsonContent');
+//  var data = {
+//      rss: 'https://medium.com/feed/@sabesan96'
+//  };
+//  $.get(' https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40hndreca', data, function (response) {
+//      if (response.status == 'ok') {
+//          $("#logo").append(`<img src="${response.feed["image"]}" class="rounded mx-auto d-block">`)
+//          var display = '';
+//          $.each(response.items, function (k, item) {
+//              display += `<div class="card mb-3 mx-auto mr-5 " style="width: 20rem;">`;
+//              var src = item["thumbnail"]; // use thumbnail url
+//              display += `<img src="${src}" class="card-img-top" alt="Cover image">`;
+//              display += `<div class="card-body">`;
+//              display += `<h5 class="card-title"><a href="${item.link}">${item.title}</a></h5>`;
+//              var yourString = item.description.replace(/<img[^>]*>/g,""); //replace with your string.
+//              yourString = yourString.replace('h4', 'p');
+//              yourString = yourString.replace('h3', 'p');
+//              var maxLength = 400; // maximum number of characters to extract
+//              //trim the string to the maximum length
+//              var trimmedString = yourString.substr(0, maxLength);
+//              //re-trim if we are in the middle of a word
+//              trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")))
 
-              display += `<p id=${itemId} class="btn-outline-success btn" >Read More</p>`;
+//              var itemId = item.guid.slice(-12);
 
-              $(document).on('click', "#"+itemId, function() {
-                trimmedString = yourString;
-              });
-              display += '</div></div>';
+//              display += `<div id=${"rl"+itemId}><p id=${"lt"+itemId} class="card-text">${trimmedString}...</p> <p id=${itemId+"m"} class="btn-outline-success btn" >Read More</p></div>`;
 
-              return k < 10;
-          });
+//              $(document).on('click', "#"+itemId+"m", function() {
+//                trimmedString = yourString;
 
-          resolve($content.html(display));
-      }
-  });
-  });
+//                $("#"+"rl"+itemId).toggleClass("hidden");
+//                $("#"+"rm"+itemId).toggleClass("hidden");
+//              });
 
-mediumPromise.then(function()
-  {
-      //Pagination
-      pageSize = 4;
+//              display += `<div id=${"rm"+itemId} class="hidden"><p id=${"mt"+itemId} class="card-text">${yourString}</p> <p id=${itemId+'l'} class="btn-outline-success btn" >Read Less</p> </div>`;
 
-      var pageCount = $(".card").length / pageSize;
+//              $(document).on('click', "#"+itemId+"l", function() {
+//                trimmedString = yourString;
 
-      for (var i = 0; i < pageCount; i++) {
-          $("#pagin").append(`<li class="page-item"><a class="page-link" href="#">${(i + 1)}</a></li> `);
-      }
-      $("#pagin li:nth-child(1)").addClass("active");
-      showPage = function (page) {
-          $(".card").hide();
-          $(".card").each(function (n) {
-              if (n >= pageSize * (page - 1) && n < pageSize * page)
-                  $(this).show();
-          });
-      }
+//                $("#"+"rl"+itemId).toggleClass("hidden");
+//                $("#"+"rm"+itemId).toggleClass("hidden");
+//              });
 
-      showPage(1);
+//              display += '</div></div>';
 
-      $("#pagin li").click(function () {
-          $("#pagin li").removeClass("active");
-          $(this).addClass("active");
-          showPage(parseInt($(this).text()))
-          return false;
-      });
-  });
-});
+//              return k < 10;
+//          });
+
+//          resolve($content.html(display));
+//      }
+//  });
+//  });
+
+//mediumPromise.then(function()
+//  {
+//      //Pagination
+//      pageSize = 4;
+
+//      var pageCount = $(".card").length / pageSize;
+
+//      for (var i = 0; i < pageCount; i++) {
+//          $("#pagin").append(`<li class="page-item"><a class="page-link" href="#">${(i + 1)}</a></li> `);
+//      }
+//      $("#pagin li:nth-child(1)").addClass("active");
+//      showPage = function (page) {
+//          $(".card").hide();
+//          $(".card").each(function (n) {
+//              if (n >= pageSize * (page - 1) && n < pageSize * page)
+//                  $(this).show();
+//          });
+//      }
+
+//      showPage(1);
+
+//      $("#pagin li").click(function () {
+//          $("#pagin li").removeClass("active");
+//          $(this).addClass("active");
+//          showPage(parseInt($(this).text()))
+//          return false;
+//      });
+//  });
+//});
--- scripts/main.js
+++ scripts/main.js
@@ -17,24 +17,28 @@ function getMobileOperatingSystem() {
 
 function appDownloadLinkGenerator() {
   let systemCheck = getMobileOperatingSystem();
-
   let appDownloadLink = "";
 
   switch (systemCheck) {
     case "Android":
       appDownloadLink =
-        "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj0ztqV5r_6AhXSXvEDHa_GC0kQFnoECA4QAQ&url=https%3A%2F%2Fapps.apple.com%2Fus%2Fapp%2Finstagram%2Fid389801252&usg=AOvVaw2-hB8WmiL6qNksivdTGruH";
+        "https://play.google.com/store/apps/details?id=com.vial.mobile";
       break;
     case "iOS":
       appDownloadLink =
-        "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj0ztqV5r_6AhXSXvEDHa_GC0kQFnoECAwQAQ&url=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dcom.instagram.android%26hl%3Den%26gl%3DUS&usg=AOvVaw2EI4stwXyzT-WkJHN9sOSe";
+        "https://apps.apple.com/al/app/vial-move-towards-web3/id6444441586";
     default:
-      appDownloadLink = "https://www.instagram.com/";
+      appDownloadLink = "https://play.google.com/store/apps/details?id=com.vial.mobile";
       break;
   }
 
+  document.getElementById('header-link').href = appDownloadLink;
+
   return appDownloadLink;
 }
+
+getMobileOperatingSystem();
+appDownloadLinkGenerator();
 
 function yesnoCheck(that) {
 
